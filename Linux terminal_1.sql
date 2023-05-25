ff2dcbaede10b120a146b00fcb85f352ec314a87
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2023-05-25 15:24:55 +0600

    Linux terminal_1

---
+++ Linux terminal_1.sql
@@ -0,0 +1,288 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 ArialMT;\f1\froman\fcharset0 Times-Roman;\f2\fswiss\fcharset0 Arial-BoldMT;
+}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red16\green60\blue192;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c6667\c33333\c80000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs29\fsmilli14667 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \uc0\u1055 \u1077 \u1088 \u1074 \u1086 \u1077  \u1044 \u1047 \'a0
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Linux terminal (GitBash) commands
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 1) \uc0\u1055 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1077 \u1090 \u1100  \u1075 \u1076 \u1077  \u1103 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~> pwd
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 2) \uc0\u1057 \u1086 \u1079 \u1076 \u1072 \u1090 \u1100  \u1087 \u1072 \u1087 \u1082 \u1091 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/terminal777> mkdir Linux_Terminal_1
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 3) \uc0\u1047 \u1072 \u1081 \u1090 \u1080  \u1074  \u1087 \u1072 \u1087 \u1082 \u1091 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/terminal777> cd Linux_Terminal_1
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 4) \uc0\u1057 \u1086 \u1079 \u1076 \u1072 \u1090 \u1100  3 \u1087 \u1072 \u1087 \u1082 \u1080 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1> mkdir \{folder1,folder2,folder3\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 5) \uc0\u1047 \u1072 \u1081 \u1090 \u1080  \u1074  \u1083 \u1102 \u1073 \u1086 \u1091 \u1102  \u1087 \u1072 \u1087 \u1082 \u1091 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1> cd folder1/
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 6) \uc0\u1057 \u1086 \u1079 \u1076 \u1072 \u1090 \u1100  5 \u1092 \u1072 \u1081 \u1083 \u1086 \u1074  (3 txt, 2 json)
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder1> touch \{file1.txt,file2.txt,file3.txt,file4.json,file5.json\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 7) \uc0\u1057 \u1086 \u1079 \u1076 \u1072 \u1090 \u1100  3 \u1087 \u1072 \u1087 \u1082 \u1080 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder1> mkdir \{folder4,folder5,folder6\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 8. \uc0\u1042 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080  \u1089 \u1087 \u1080 \u1089 \u1086 \u1082  \u1089 \u1086 \u1076 \u1077 \u1088 \u1078 \u1080 \u1084 \u1086 \u1075 \u1086  \u1087 \u1072 \u1087 \u1082 \u1080 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder1> ls -la
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 9) + \uc0\u1054 \u1090 \u1082 \u1088 \u1099 \u1090 \u1100  \u1083 \u1102 \u1073 \u1086 \u1081  txt \u1092 \u1072 \u1081 \u1083 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder1> cat >> file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 10) + \uc0\u1085 \u1072 \u1087 \u1080 \u1089 \u1072 \u1090 \u1100  \u1090 \u1091 \u1076 \u1072  \u1095 \u1090 \u1086 -\u1085 \u1080 \u1073 \u1091 \u1076 \u1100 , \u1083 \u1102 \u1073 \u1086 \u1081  \u1090 \u1077 \u1082 \u1089 \u1090 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 sheet1
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 11) + \uc0\u1089 \u1086 \u1093 \u1088 \u1072 \u1085 \u1080 \u1090 \u1100  \u1080  \u1074 \u1099 \u1081 \u1090 \u1080 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 control+C
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 12) \uc0\u1042 \u1099 \u1081 \u1090 \u1080  \u1080 \u1079  \u1087 \u1072 \u1087 \u1082 \u1080  \u1085 \u1072  \u1091 \u1088 \u1086 \u1074 \u1077 \u1085 \u1100  \u1074 \u1099 \u1096 \u1077 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder1> cd ../
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \'97
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 13) \uc0\u1087 \u1077 \u1088 \u1077 \u1084 \u1077 \u1089 \u1090 \u1080 \u1090 \u1100  \u1083 \u1102 \u1073 \u1099 \u1077  2 \u1092 \u1072 \u1081 \u1083 \u1072 , \u1082 \u1086 \u1090 \u1086 \u1088 \u1099 \u1077  \u1074 \u1099  \u1089 \u1086 \u1079 \u1076 \u1072 \u1083 \u1080 , \u1074  \u1083 \u1102 \u1073 \u1091 \u1102  \u1076 \u1088 \u1091 \u1075 \u1091 \u1102  \u1087 \u1072 \u1087 \u1082 \u1091 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1 [1]> mv /Users/Vadim/terminal777/Linux_Terminal_1/folder1/file1.txt /Users/Vadim/terminal777/Linux_Terminal_1/folder1/file2.txt folder2/
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 14) \uc0\u1089 \u1082 \u1086 \u1087 \u1080 \u1088 \u1086 \u1074 \u1072 \u1090 \u1100  \u1083 \u1102 \u1073 \u1099 \u1077  2 \u1092 \u1072 \u1081 \u1083 \u1072 , \u1082 \u1086 \u1090 \u1086 \u1088 \u1099 \u1077  \u1074 \u1099  \u1089 \u1086 \u1079 \u1076 \u1072 \u1083 \u1080 , \u1074  \u1083 \u1102 \u1073 \u1091 \u1102  \u1076 \u1088 \u1091 \u1075 \u1091 \u1102  \u1087 \u1072 \u1087 \u1082 \u1091 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1> cp /Users/Vadim/terminal777/Linux_Terminal_1/folder1/file4.json /Users/Vadim/terminal777/Linux_Terminal_1/folder1/file5.json folder2/
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 15) \uc0\u1053 \u1072 \u1081 \u1090 \u1080  \u1092 \u1072 \u1081 \u1083  \u1087 \u1086  \u1080 \u1084 \u1077 \u1085 \u1080 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1 [SIGINT]> find . -name file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 16) \uc0\u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1077 \u1090 \u1100  \u1089 \u1086 \u1076 \u1077 \u1088 \u1078 \u1080 \u1084 \u1086 \u1077  \u1074  \u1088 \u1077 \u1072 \u1083 \u1100 \u1085 \u1086 \u1084  \u1074 \u1088 \u1077 \u1084 \u1077 \u1085 \u1080  (\u1082 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  grep) \u1080 \u1079 \u1091 \u1095 \u1080 \u1090 \u1077  \u1082 \u1072 \u1082  \u1086 \u1085 \u1072  \u1088 \u1072 \u1073 \u1086 \u1090 \u1072 \u1077 \u1090 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1045 \u1089 \u1090 \u1100  \u1085 \u1077 \u1089 \u1082 \u1086 \u1083 \u1100 \u1082 \u1086  \u1074 \u1072 \u1088 \u1080 \u1072 \u1085 \u1090 \u1086 \u1074  \u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1077 \u1090 \u1100  \u1089 \u1086 \u1076 \u1077 \u1088 \u1078 \u1080 \u1084 \u1086 \u1077  \u1092 \u1072 \u1081 \u1083 \u1072 :
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> cat file1.txt
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> nl file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1050 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  
+\f2\b nl
+\f0\b0  \uc0\u1088 \u1072 \u1073 \u1086 \u1090 \u1072 \u1077 \u1090  \u1082 \u1072 \u1082  \u1080  
+\f2\b cat
+\f0\b0 , \uc0\u1085 \u1086  \u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1077 \u1097 \u1077  \u1080  \u1085 \u1086 \u1084 \u1077 \u1088 \u1072  \u1089 \u1090 \u1088 \u1086 \u1082  \u1074  \u1089 \u1090 \u1086 \u1083 \u1073 \u1094 \u1077  \u1089 \u1083 \u1077 \u1074 \u1072 .
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2 [SIGINT]> less file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1050 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  
+\f2\b less 
+\f0\b0 \uc0\u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1089 \u1086 \u1076 \u1077 \u1088 \u1078 \u1080 \u1084 \u1086 \u1077  \u1092 \u1072 \u1081 \u1083 \u1072 , \u1085 \u1086  \u1086 \u1090 \u1086 \u1073 \u1088 \u1072 \u1078 \u1072 \u1077 \u1090  \u1077 \u1075 \u1086  \u1090 \u1086 \u1083 \u1100 \u1082 \u1086  \u1074  \u1088 \u1072 \u1084 \u1082 \u1072 \u1093  \u1090 \u1077 \u1082 \u1091 \u1097 \u1077 \u1075 \u1086  \u1086 \u1082 \u1085 \u1072  \u1074  \u1088 \u1077 \u1078 \u1080 \u1084 \u1077  \u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1072 . \u1053 \u1077 \'a0 \u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1090 \u1077 \u1082 \u1089 \u1090  \u1092 \u1072 \u1081 \u1083 \u1072  \u1074  \u1090 \u1077 \u1088 \u1084 \u1080 \u1085 \u1072 \u1083 \u1077 .
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> more file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1050 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  
+\f2\b more
+\f0\b0  \uc0\u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1092 \u1072 \u1081 \u1083  \u1074  \u1090 \u1077 \u1088 \u1084 \u1080 \u1085 \u1072 \u1083 \u1077  \u1074  \u1088 \u1077 \u1078 \u1080 \u1084 \u1077  \u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1072 , \u1085 \u1086  \u1074  \u1086 \u1090 \u1083 \u1080 \u1095 \u1080 \u1077  \u1086 \u1090  
+\f2\b less
+\f0\b0  \uc0\u1086 \u1089 \u1090 \u1072 \u1074 \u1083 \u1103 \u1077 \u1090  \u1090 \u1077 \u1082 \u1089 \u1090  \u1092 \u1072 \u1081 \u1083 \u1072  \u1074  \u1090 \u1077 \u1088 \u1084 \u1080 \u1085 \u1072 \u1083 \u1077 
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> head file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1050 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  
+\f2\b head
+\f0\b0  \uc0\u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1087 \u1077 \u1088 \u1074 \u1099 \u1077  10 \u1089 \u1090 \u1088 \u1086 \u1082  \u1092 \u1072 \u1081 \u1083 \u1072 . \u1057  \u1087 \u1086 \u1084 \u1086 \u1097 \u1100 \u1102  \u1086 \u1087 \u1094 \u1080 \u1080  
+\f2\b n
+\f0\b0  \uc0\u1084 \u1086 \u1078 \u1085 \u1086  \u1079 \u1072 \u1076 \u1072 \u1090 \u1100  \u1082 \u1086 \u1083 \u1080 \u1095 \u1077 \u1089 \u1090 \u1074 \u1086  \u1089 \u1090 \u1088 \u1086 \u1082 , \u1082 \u1086 \u1090 \u1086 \u1088 \u1086 \u1077  \u1085 \u1091 \u1078 \u1085 \u1086  \u1074 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1053 \u1072 \u1087 \u1088 \u1080 \u1084 \u1077 \u1088  \u1082 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  
+\f2\b head -2 file1.txt
+\f0\b0  \uc0\u1074 \u1099 \u1074 \u1077 \u1076 \u1077 \u1090  \u1087 \u1077 \u1088 \u1074 \u1099 \u1077  2 \u1089 \u1090 \u1088 \u1086 \u1082 \u1080  \u1092 \u1072 \u1081 \u1083 \u1072 .
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> tail file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1050 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  tail \u1074 \u1099 \u1074 \u1086 \u1076 \u1080 \u1090  \u1087 \u1086 \u1089 \u1083 \u1077 \u1076 \u1085 \u1080 \u1077  10 \u1089 \u1090 \u1088 \u1086 \u1082  \u1092 \u1072 \u1081 \u1083 \u1072 . \u1057  \u1087 \u1086 \u1084 \u1086 \u1097 \u1100 \u1102  \u1086 \u1087 \u1094 \u1080 \u1080  
+\f2\b n
+\f0\b0  \uc0\u1084 \u1086 \u1078 \u1085 \u1086  \u1079 \u1072 \u1076 \u1072 \u1090 \u1100  \u1082 \u1086 \u1083 \u1080 \u1095 \u1077 \u1089 \u1090 \u1074 \u1086  \u1089 \u1090 \u1088 \u1086 \u1082 , \u1082 \u1086 \u1090 \u1086 \u1088 \u1086 \u1077  \u1085 \u1091 \u1078 \u1085 \u1086  \u1074 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080  (\u1089 \u1095 \u1080 \u1090 \u1072 \u1077 \u1090 \u1089 \u1103  \u1086 \u1090  \u1087 \u1086 \u1089 \u1083 \u1077 \u1076 \u1085 \u1077 \u1081  \u1089 \u1090 \u1088 \u1086 \u1082 \u1080 ).
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 17) \uc0\u1074 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080  \u1085 \u1077 \u1089 \u1082 \u1086 \u1083 \u1100 \u1082 \u1086  \u1087 \u1077 \u1088 \u1074 \u1099 \u1093  \u1089 \u1090 \u1088 \u1086 \u1082  \u1080 \u1079  \u1090 \u1077 \u1082 \u1089 \u1090 \u1086 \u1074 \u1086 \u1075 \u1086  \u1092 \u1072 \u1081 \u1083 \u1072 
+\f1\fs24 \
+\pard\pardeftab720\partightenfactor0
+
+\f2\b\fs29\fsmilli14667 \cf0 head -2 file1.txt
+\f1\b0\fs24 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs29\fsmilli14667 \cf0 18) \uc0\u1074 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080  \u1085 \u1077 \u1089 \u1082 \u1086 \u1083 \u1100 \u1082 \u1086  \u1087 \u1086 \u1089 \u1083 \u1077 \u1076 \u1085 \u1080 \u1093  \u1089 \u1090 \u1088 \u1086 \u1082  \u1080 \u1079  \u1090 \u1077 \u1082 \u1089 \u1090 \u1086 \u1074 \u1086 \u1075 \u1086  \u1092 \u1072 \u1081 \u1083 \u1072 
+\f1\fs24 \
+\pard\pardeftab720\partightenfactor0
+
+\f2\b\fs29\fsmilli14667 \cf0 tail -2 file1.txt
+\f1\b0\fs24 \
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs29\fsmilli14667 \cf0 19) \uc0\u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1077 \u1090 \u1100  \u1089 \u1086 \u1076 \u1077 \u1088 \u1078 \u1080 \u1084 \u1086 \u1077  \u1076 \u1083 \u1080 \u1085 \u1085 \u1086 \u1075 \u1086  \u1092 \u1072 \u1081 \u1083 \u1072  (\u1082 \u1086 \u1084 \u1072 \u1085 \u1076 \u1072  less) \u1080 \u1079 \u1091 \u1095 \u1080 \u1090 \u1077  \u1082 \u1072 \u1082  \u1086 \u1085 \u1072  \u1088 \u1072 \u1073 \u1086 \u1090 \u1072 \u1077 \u1090 .
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2 [SIGINT]> less file1.txt
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1055 \u1088 \u1086 \u1082 \u1088 \u1091 \u1095 \u1080 \u1074 \u1072 \u1090 \u1100  \u1090 \u1077 \u1082 \u1089 \u1090  \u1092 \u1072 \u1081 \u1083 \u1072  \u1082 \u1083 \u1072 \u1074 \u1080 \u1096 \u1072 \u1084 \u1080  
+\f2\b w
+\f0\b0  \uc0\u1080  
+\f2\b z
+\f0\b0 \'a0
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1055 \u1086 \u1080 \u1089 \u1082  \u1090 \u1077 \u1082 \u1089 \u1090 \u1072  \u1074 \u1085 \u1091 \u1090 \u1088 \u1080  \u1092 \u1072 \u1081 \u1083 \u1072  
+\f2\b /
+\f1\b0\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1055 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088  \u1089 \u1087 \u1080 \u1089 \u1082 \u1072  \u1076 \u1086 \u1089 \u1090 \u1091 \u1087 \u1085 \u1099 \u1093  \u1082 \u1086 \u1084 \u1072 \u1085 \u1076  
+\f2\b h
+\f1\b0\fs24 \
+
+\f0\fs29\fsmilli14667 \uc0\u1042 \u1099 \u1081 \u1090 \u1080  \u1080 \u1079  \u1088 \u1077 \u1078 \u1080 \u1084 \u1072  \u1087 \u1088 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1072  
+\f2\b q
+\f1\b0\fs24 \
+
+\f0\fs29\fsmilli14667 20) \uc0\u1074 \u1099 \u1074 \u1077 \u1089 \u1090 \u1080  \u1076 \u1072 \u1090 \u1091  \u1080  \u1074 \u1088 \u1077 \u1084 \u1103 
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/L/folder2> date
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 =========
+\f1\fs24 \
+\
+
+\f0\fs29\fsmilli14667 \uc0\u1047 \u1072 \u1076 \u1072 \u1085 \u1080 \u1077  *
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 1) \uc0\u1054 \u1090 \u1087 \u1088 \u1072 \u1074 \u1080 \u1090 \u1100  http \u1079 \u1072 \u1087 \u1088 \u1086 \u1089  \u1085 \u1072  \u1089 \u1077 \u1088 \u1074 \u1077 \u1088 .
+\f1\fs24 \
+\pard\pardeftab720\partightenfactor0
+{\field{\*\fldinst{HYPERLINK "http://162.55.220.72:5005/terminal-hw-request"}}{\fldrslt 
+\f0\fs29\fsmilli14667 \cf3 \ul \ulc3 \strokec3 http://162.55.220.72:5005/terminal-hw-request}}\
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs29\fsmilli14667 \cf0 Vadim@MacBook-Pro-newowner ~/t/L/folder2> curl http://162.55.220.72:5005/terminal-hw-request
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 2) \uc0\u1053 \u1072 \u1087 \u1080 \u1089 \u1072 \u1090 \u1100  \u1089 \u1082 \u1088 \u1080 \u1087 \u1090  \u1082 \u1086 \u1090 \u1086 \u1088 \u1099 \u1081  \u1074 \u1099 \u1087 \u1086 \u1083 \u1085 \u1080 \u1090  \u1072 \u1074 \u1090 \u1086 \u1084 \u1072 \u1090 \u1080 \u1095 \u1077 \u1089 \u1082 \u1080  \u1087 \u1091 \u1085 \u1082 \u1090 \u1099  3, 4, 5, 6, 7, 8, 13
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 Vadim@MacBook-Pro-newowner ~/t/Linux_Terminal_1> vim script.sh
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 cd ../
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 mkdir Linux_Terminal_2
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 cd Linux_Terminal_2
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 mkdir \{folder1,folder2,folder3\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 cd folder1/
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 touch \{file1.txt,file2.txt,file3.txt,file4.json,file5.json\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 mkdir \{folder4,folder5,folder6\}
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 ls -la
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 mv file1.txt file2.txt ../folder2
+\f1\fs24 \
+\
+\
+
+\f0\fs29\fsmilli14667 =====================
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 1) \uc0\u1055 \u1086 \u1089 \u1084 \u1086 \u1090 \u1088 \u1077 \u1090 \u1100  \u1075 \u1076 \u1077  \u1103  - pwd
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 2) \uc0\u1057 \u1086 \u1079 \u1076 \u1072 \u1090 \u1100  \u1087 \u1072 \u1087 \u1082 \u1091  - mkdir foldername
+\f1\fs24 \
+
+\f0\fs29\fsmilli14667 3) \uc0\u1047 \u1072 \u1081 \u1090 \u1080  \u1074  \u1087 \u1072 \u1087 \u1082 \u1091  - cd foldername
+\f1\fs24 \
+\
+}
