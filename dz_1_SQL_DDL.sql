0d31be647fbdfb3e9129e94725fcddb29611fc6f
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2023-07-21 20:08:40 +0600

    Update dz_1_SQL_DDL.sql

--- dz_1_SQL_DDL.sql
+++ dz_1_SQL_DDL.sql
@@ -1,7 +1,13 @@
+/Таблица employees
+
+/Создать таблицу employees
+
 create table employees (
 	id serial primary key,
 	employee_name varchar(50) not null
 );
+
+/Наполнить таблицу employee 70 строками.
 
 insert into employees
 values ('Olivia'),
@@ -76,14 +82,45 @@ values ('Olivia'),
 		('David');
 
 
-
+/ Вывести таблицу employees
 select * from employees;
+
+Таблица salary
+
+Создать таблицу salary
 
 create table salary (
 	id serial primary key,
 	monthly_salary int not null
 );
 
+/ Наполнить таблицу salary 15 строками:
+
+insert into salary (monthly_salary)
+values (1000),
+(1100),
+(1200),
+(1300),
+(1400),
+(1500),
+(1600),
+(1700),
+(1800),
+(1900),
+(2000),
+(2100),
+(2200),
+(2300),
+(2400),
+(2500);
+
+/Вывести таблицу salary
+select * from salary
+
+/Таблица employee_salary
+
+/Создать таблицу employee_salary
+	
 create table employee_salary (
 	id serial primary key,
 	employee_id int not null unique,
@@ -94,11 +131,89 @@ create table employee_salary (
 		references salary(id)
 );
 
+/Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
+
+update into employee_salary (employee_id, salary_id)
+values (1, 1),
+		(2, 2),
+		(3, 3),
+		(4, 4),
+		(5, 5),
+		(6, 6),
+		(7, 7),
+		(8, 8),
+		(9, 9),
+		(10, 10),
+		(11, 11),
+		(12, 12),
+		(13, 13),
+		(14, 14),
+		(15, 15),
+		(16, 1),
+		(17, 2),
+		(18, 3),
+		(19, 4),
+		(20, 5),
+		(21, 6),
+		(22, 7),
+		(23, 8),
+		(24, 9),
+		(25, 10),
+		(26, 11),
+		(27, 12),
+		(28, 13),
+		(29, 14),
+		(30, 15);
+
+/Таблица roles
+
+/Создать таблицу roles
+
+
 create table roles (
 	id serial primary key,
 	role_name int not null unique
 );
 
+/Поменять тип столба role_name с int на varchar(30)
+
+alter table roles
+alter column role_name type varchar(30);
+
+/Наполнить таблицу roles 20 строками:
+
+insert into roles (role_name)
+values ('Junior Python developer'),
+('Middle Python developer'),
+('Senior Python developer'),
+('Junior Java developer'),
+('Middle Java developer'),
+('Senior Java developer'),
+('Junior JavaScript developer'),
+('Middle JavaScript developer'),
+('Senior JavaScript developer'),
+('Junior Manual QA engineer'),
+('Middle Manual QA engineer'),
+('Senior Manual QA engineer'),
+('Project Manager'),
+('Designer'),
+('HR'),
+('CEO'),
+('Sales manager'),
+('Junior Automation QA engineer'),
+('Middle Automation QA engineer'),
+('Senior Automation QA engineer');
+
+/Вывести таблицу roles
+
+select * from roles
+
+/Таблица roles_employee
+
+/Создать таблицу roles_employee
+
+	
 create table roles_employee (
 	id serial primary key,
 	employee_id int not null unique,
@@ -108,6 +223,8 @@ create table roles_employee (
 	foreign key (role_id)
 		references roles(id)
 );
+
+/Наполнить таблицу roles_employee 40 строками:
 
 insert into roles_employee (employee_id, role_id)
 values (1, 20),
@@ -151,175 +268,8 @@ values (1, 20),
 		(39,	2),
 		(40,	1);
 
+/Вывести таблицу roles_employee
 select * from roles_employee
-join employees on employee_id = employees.id
-join roles on role_id = roles.id;
 
 
-
-alter table roles 
-	alter column role_name type varchar(30);
-
-insert into roles (role_name)
-values ('Junior Python developer'),
-('Middle Python developer'),
-('Senior Python developer'),
-('Junior Java developer'),
-('Middle Java developer'),
-('Senior Java developer'),
-('Junior JavaScript developer'),
-('Middle JavaScript developer'),
-('Senior JavaScript developer'),
-('Junior Manual QA engineer'),
-('Middle Manual QA engineer'),
-('Senior Manual QA engineer'),
-('Project Manager'),
-('Designer'),
-('HR'),
-('CEO'),
-('Sales manager'),
-('Junior Automation QA engineer'),
-('Middle Automation QA engineer'),
-('Senior Automation QA engineer');
-
-
-
-
-
-
-
-update into employee_salary (employee_id, salary_id)
-values (1, 1),
-		(2, 2),
-		(3, 3),
-		(4, 4),
-		(5, 5),
-		(6, 6),
-		(7, 7),
-		(8, 8),
-		(9, 9),
-		(10, 10),
-		(11, 11),
-		(12, 12),
-		(13, 13),
-		(14, 14),
-		(15, 15),
-		(16, 1),
-		(17, 2),
-		(18, 3),
-		(19, 4),
-		(20, 5),
-		(21, 6),
-		(22, 7),
-		(23, 8),
-		(24, 9),
-		(25, 10),
-		(26, 11),
-		(27, 12),
-		(28, 13),
-		(29, 14),
-		(30, 15);
-	
-
-insert into salary (monthly_salary)
-values (1000),
-(1100),
-(1200),
-(1300),
-(1400),
-(1500),
-(1600),
-(1700),
-(1800),
-(1900),
-(2000),
-(2100),
-(2200),
-(2300),
-(2400),
-(2500);
-
-
-
-
-insert into employees (employee_name)
-values ('Olivia'),
-('Ethan'),
-('Ava'),
-('Liam'),
-('Sophia'),
-('Noah'),
-('Isabella'),
-('Jackson'),
-('Mia'),
-('Aiden'),
-('Charlotte'),
-('Lucas'),
-('Harper'),
-('Elijah'),
-('Amelia'),
-('Mason'),
-('Evelyn'),
-('Caden'),
-('Abigail'),
-('Grayson'),
-('Scarlett'),
-('Declan'),
-('Grace'),
-('Wyatt'),
-('Chloe'),
-('James'),
-('Zoe'),
-('Alexander'),
-('Penelope'),
-('William'),
-('Layla'),
-('Michael'),
-('Hazel'),
-('Benjamin'),
-('Aria'),
-('Elijah'),
-('Aubrey'),
-('Nathan'),
-('Riley'),
-('Logan'),
-('Ella'),
-('Gabriel'),
-('Lily'),
-('Henry'),
-('Nora'),
-('Owen'),
-('Savannah'),
-('Dylan'),
-('Brooklyn'),
-('Jayden'),
-('Makayla'),
-('Caleb'),
-('Kennedy'),
-('Ryan'),
-('Paisley'),
-('Isaac'),
-('Faith'),
-('Samuel'),
-('Vivienne'),
-('Julian'),
-('Adalynn'),
-('Dylan'),
-('Skylar'),
-('Levi'),
-('Elizabeth'),
-('Christian'),
-('Mila'),
-('Hunter'),
-('Addison'),
-('David');
-
-
-
-
-select * from roles;
-
-select * from employee_salary
-join employees on employee_id = employees.id
-join salary on salary_id = salary.id
 
